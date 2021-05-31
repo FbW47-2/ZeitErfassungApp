172d7fd5a5f7dfd107049687a24f579e6b475e1b
ZeynepGultekin <zeynep.pehlivan@windowslive.com>
2021-05-31 15:36:16 +0200

    Employees.js,Employees.css added and InputGroup Button and ListGroup created

--- zeiterfassung/src/Pages/ShowPersonal.jsx
+++ zeiterfassung/src/Pages/ShowPersonal.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Employees from '../Component/Employees'
 
 function ShowPersonal() {
     return (
         <div>
             <p>ShowPersonal</p>
-            <p>Zeynep</p>
+            <Employees/>
         </div>
     )
 }
