24ce97842b341f278c8544beeb574c9919b6652c
Galip <galipsvt16@gmail.com>
2021-05-31 23:20:16 +0200

    template

--- zeiterfassung/src/Pages/Payment.jsx
+++ zeiterfassung/src/Pages/Payment.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Payment() {
     return (
         <div>
-            <p>Paymentttttttttttttttttt</p>
+            <h1>Cafer</h1>
+            <p>23.05.2021 - 26.05.2021 tarihleri arasinda</p>
+            <p>15 saat 40 dk calismistir.</p>
+            <p>alacagi maas 250€</p>
         </div>
     )
 }
