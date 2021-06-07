b4d8b6e0e84dac45d62abfe5340b5dda1b28c605
ahmadaflak8 <ahmadaflak8@gmail.com>
2021-06-07 09:58:40 +0200

    änderung

--- zeiterfassung/src/App.js
+++ zeiterfassung/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
        <BrowserRouter>
-        <NavBar />
+        
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/showpersonal" component={ShowPersonal} />
