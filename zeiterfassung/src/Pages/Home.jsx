5eb4ec0599ff03d5fb680bd751a9544edb68a592
Lamia Gülsah <guelsah-lamia@gmx.de>
2021-06-07 09:53:41 +0200

    loggin

--- zeiterfassung/src/Pages/Home.jsx
+++ zeiterfassung/src/Pages/Home.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import "./Home.css";
+import { FormGroup, Form, Button, Input, Col } from "reactstrap";
+import { useState, useHistory } from "react";
+import { LoggInContext } from "../Context/LoggInContext";
 
 function Home() {
+
+        
+
+        const [ password, controlPassword ] = useState(false);
+
+        // const handleLogin = () => {
+        //     //hier wird bei richtigen Anmeldedaten die Info an context gesendet
+        //     useContext({name: {"Galip"}, loggedin: true})
+        // }
+
+        // (Context-Komponente
+        //     //hier wird der user verwaltet
+        //     // bekommt info von Login/Home Komponent, durch handleLogin() Funktion
+        //     //schickt info an die benötigten komponenten
+
+
+        // )
+
+        const { setUsername, setShowProfile, setPassword } = useContext(LoggInContext); 
+
     return (
-        <div>
-            <p>Home</p>
+        <div className="Container">
+            <Form className="Login FormFields">
+                <div className = "LogInput">
+
+                    <FormGroup row>
+
+                        <Col sm={10}>
+                            <Input className="inputField" size="lg" type="name" name="name" id="name" placeholder="Name" onChange={ (e) => { setUsername(e.target.value)}}/>
+                        </Col>
+
+                    </FormGroup>
+                
+                    <FormGroup row>
+       
+                        <Col sm={10}>
+                            <Input className="inputField" size="lg" type="password" name="pw" id="pw" placeholder="Password" onChange = { (e) => setPassword(e.target.value) } />
+                        </Col>
+
+                    </FormGroup>
+                    
+                    <Button sm={10} color="success" size="lg" block onClick = { () => { setShowProfile(true) }}>Log in</Button>
+
+                </div>
+    
+            </Form>
         </div>
     )
 }
---
+++ zeiterfassung/src/Pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from "../NavBar"
+
+function Homepage() {
+    return (
+
+        <>
+        <Navbar/>
+
+        <div>
+
+            <p>this is the homepage</p>
+        </div>
+        
+        </>
+    )
+}
+
+export default Homepage
