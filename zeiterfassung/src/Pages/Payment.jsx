9729fd5ddb484cdf403698cab871bda80d0c0a49
Galip <galipsvt16@gmail.com>
2021-06-07 09:54:00 +0200

    payment updated

--- zeiterfassung/src/Pages/Payment.jsx
+++ zeiterfassung/src/Pages/Payment.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import NavBar from "../NavBar"
 
 function Payment() {
     return (
+       <>
+       <NavBar/>
         <div>
             <h1>Cafer</h1>
             <p>23.05.2021 - 26.05.2021 tarihleri arasinda</p>
             <p>15 saat 40 dk calismistir.</p>
             <p>alacagi maas 250€</p>
         </div>
+       </>
     )
 }
 
